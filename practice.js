7f8fe1dc767f0b1a96da093f06feb02e600a9e86
ebenofere <eofere@gmail.com>
2024-01-18 01:28:45 -0500

    update

--- practice.js
+++ practice.js
@@ -1,5 +1,26 @@
-let array = ["Messi", "Haaland", "Osimhen", "De Bruyne", "Benzema", "Modric"];
+function XO(str) {
+    str = str.toLowerCase();
 
-let number = array.map((item, index) => `${index + 1} : ${item}`);
+    let countX = 0;
+    let countO = 0;
 
-console.log(number, "number");
+    for (let char of str) {
+        if (char === 'x') {
+            countX++;
+        } else if (char === 'o') {
+            countO++;
+        }
+    }
+
+    return countX == countO;
+}
+
+console.log(XO("ooxx"));
+console.log(XO("zzoo"));
+
+// Examples input/output:
+XO("ooxx") // => true
+XO("xooxx") // => false
+XO("ooxXm") // => true
+XO("zpzpzpp") // => true // when no 'x' and 'o' is present should return true
+XO("zzoo") // => false
